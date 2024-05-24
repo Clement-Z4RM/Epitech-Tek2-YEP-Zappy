a1d25f1590819e8a42df01b3fb1f84f70e819880
Clement-Z4RM <clement.lienard@epitech.eu>
2024-05-25 00:16:37 +0200

    feat(server-options): add `options_t` constructor and destructor and a start of `parse_options` function

--- server/src/modules/options/options.c
+++ server/src/modules/options/options.c
@@ -5,4 +5,56 @@
 ** options.c
 */
 
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 #include "options.h"
+
+static options_t *options_constructor(void)
+{
+    options_t *options = malloc(sizeof(options_t));
+
+    if (!options)
+        return NULL;
+    memset(options, 0, sizeof(options_t));
+    SLIST_INIT(&options->teams);
+    return options;
+}
+
+void options_destructor(options_t *options)
+{
+    team_name_t *team_name;
+
+    if (!options)
+        return;
+    while (!SLIST_EMPTY(&options->teams)) {
+        team_name = SLIST_FIRST(&options->teams);
+        SLIST_REMOVE_HEAD(&options->teams, next);
+        free(team_name);
+    }
+    free(options);
+}
+
+/**
+ * @param argc
+ * @param argv
+ * @return
+ */
+options_t *parse_options(int argc, char *argv[])
+{
+    options_t *options = options_constructor();
+    int opt;
+
+    if (!options)
+        return NULL;
+    for (opt = NEXT_OPT(argc, argv); -1 != opt; opt = NEXT_OPT(argc, argv)) {
+        if ('?' == opt) {
+            options_destructor(options);
+            return NULL;
+        }
+        if (1 == opt) {
+            // TODO
+        }
+    }
+    return options;
+}
