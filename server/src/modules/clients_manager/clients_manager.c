a5b323070a2cd93e92cfe0b6d3ac6d1cb25eb65a
RemFdz <remi.fernandez@epitech.eu>
2024-06-03 18:53:12 +0200

    refactor(client_manager): using SLIST for the linked list of clients

--- server/src/modules/clients_manager/clients_manager.c
+++ server/src/modules/clients_manager/clients_manager.c
@@ -7,58 +7,60 @@
 
 #include "clients_manager.h"
 #include <stdlib.h>
+#include "sys/queue.h"
+#include "stdio.h"
 
-void client_manager_add_client(client_manager_t *manager, client_t *client)
+void client_manager_add(client_manager_t *manager, client_t *client)
 {
     client_node_t *new_node = malloc(sizeof(client_node_t));
 
     if (!new_node)
         return;
     new_node->client = client;
-    new_node->next = manager->clients_list;
-    manager->clients_list = new_node;
+    SLIST_INSERT_HEAD(&manager->clients_list, new_node, next);
     manager->nb_clients++;
 }
 
-void client_manager_remove_client(client_manager_t *manager, client_t *client)
+void client_manager_remove(client_manager_t *manager, client_t *client)
 {
-    client_node_t *current = manager->clients_list;
-    client_node_t *prev = NULL;
+    client_node_t *current = NULL;
+    client_node_t *tmp = NULL;
 
-    while (current) {
-        if (current->client == client)
+    for (current = SLIST_FIRST(&manager->clients_list); current;
+        current = SLIST_NEXT(current, next)) {
+        if (current->client == client) {
+            tmp = current;
             break;
-        prev = current;
-        current = current->next;
+        }
     }
-    if (!current)
-        return;
-    if (prev)
-        prev->next = current->next;
-    else
-        manager->clients_list = current->next;
+    if (tmp) {
+        SLIST_REMOVE(&manager->clients_list, tmp, client_node_s, next);
+        client_destroy(tmp->client);
+        free(tmp);
+        manager->nb_clients--;
+    }
 }
 
-void client_manager_destroy(client_manager_t *manager)
+void client_manager_destructor(client_manager_t *manager)
 {
-    client_node_t *next = NULL;
+    client_node_t *current = NULL;
 
-    for (client_node_t *current = manager->clients_list; current;) {
-        next = current->next;
+    while (!SLIST_EMPTY(&manager->clients_list)) {
+        current = SLIST_FIRST(&manager->clients_list);
+        SLIST_REMOVE_HEAD(&manager->clients_list, next);
         client_destroy(current->client);
         free(current);
-        current = next;
     }
     free(manager);
 }
 
-client_manager_t *client_manager_create(void)
+client_manager_t *client_manager_constructor(void)
 {
     client_manager_t *manager = malloc(sizeof(client_manager_t));
 
     if (!manager)
         return NULL;
-    manager->clients_list = NULL;
+    SLIST_INIT(&manager->clients_list);
     manager->nb_clients = 0;
     return manager;
 }
