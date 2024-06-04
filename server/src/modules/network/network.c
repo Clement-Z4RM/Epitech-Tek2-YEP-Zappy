541ef8b2ec5ea77d6e375cae97a7ee1b1a7b31bd
RemFdz <remi.fernandez@epitech.eu>
2024-06-04 16:06:56 +0200

    feat(server-network): network module used to represent to network core of the server

--- server/src/modules/network/network.c
+++ server/src/modules/network/network.c
@@ -6,3 +6,25 @@
 */
 
 #include "network.h"
+
+void network_destructor(network_t *network)
+{
+    if (network->endpoint)
+        endpoint_destructor(network->endpoint);
+    free(network);
+}
+
+network_t *network_constructor(char *ip, int port)
+{
+    network_t *network = malloc(sizeof(network_t));
+
+    if (network == NULL)
+        return NULL;
+    network->endpoint = endpoint_constructor(ip, port, SERVER);
+    if (network->endpoint == NULL) {
+        free(network);
+        return NULL;
+    }
+    network->clients_manager = client_manager_constructor();
+    return network;
+}
--- server/src/modules/network/network.h
+++ server/src/modules/network/network.h
@@ -6,9 +6,20 @@
 */
 
 #pragma once
-
+#include "stdlib.h"
 #include "endpoint/endpoint.h"
+#include "sys/select.h"
+#include "clients_manager/clients_manager.h"
 
+/**
+* @brief represent the network module
+* @description
+* The network module is responsible for creating and managing the network
+* (sockets and connections)
+**/
 typedef struct network_s {
-    endpoint_t *endpoint;
+    endpoint_t *endpoint; //< the endpoint of the network
+    fd_set read_fds; //< the file descriptor set for reading
+    fd_set write_fds; //< the file descriptor set for writing
+    client_manager_t *clients_manager; //< the clients manager
 } network_t;
