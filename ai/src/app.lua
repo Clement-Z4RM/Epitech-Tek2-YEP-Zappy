eef3c3d64e0b1a341a1061fe5bc9578f72b53390
Pablo ZAPATA <pablo.zapata@epitech.eu>
2024-06-07 16:41:24 +0200

    tweak(ai/core): updated the program's routine to make it more understandable

--- ai/src/app.lua
+++ ai/src/app.lua
@@ -1,10 +1,10 @@
 local Socket <const> = require("socket")
 local ZappyAI <const> = require("ai/src/classes/zappy_ai")
-local Config <const> = require("ai/src/config")
 
 --- @module App
 local App = {}
 
+--- @return void
 function App.ShowHelp()
     print("USAGE:\t./zappy_ai -p port -n name -h machine")
 end
@@ -15,14 +15,13 @@ function App.Main(args)
     if #args > 0 and args[1] == "-help" then
         return App.ShowHelp()
     end
-    local zappyAI <const> = ZappyAI.New(args)
-    
-end
-
---- @param code number
---- @return void
-function App.Exit(code)
-    os.exit(code)
+    local ai <const> = ZappyAI.New(args)
+    if not ai.params:ContainsAll({"h", "p", "n"}) then
+        return ai:Error("Too few arguments, see -help to see usage.")
+    end
+    if not ai.server:Connect() then
+        return ai:Error("Could not connect to the Zappy server.")
+    end
 end
 
 return App
