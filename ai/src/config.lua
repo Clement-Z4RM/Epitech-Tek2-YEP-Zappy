509403666860492ea84c6dd1e4c60a824976569a
Pablo ZAPATA <pablo.zapata@epitech.eu>
2024-06-07 16:39:29 +0200

    delete(ai/config): remove the configuration file content

--- ai/src/config.lua
+++ ai/src/config.lua
@@ -1,20 +1,5 @@
 local Config <const> = {
-    arguments = {
-        {
-            identifier = "p",
-            name = "port",
-            isNumber = true,
-        },
-        {
-            identifier = "n",
-            name = "name"
-        },
-        {
-            identifier = "h",
-            name = "host",
-            isNumber = true,
-        }
-    }
+   
 }
 
 return Config
