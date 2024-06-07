c00bf151607e93a14bb38197ce5d2fcea9af0559
Pablo ZAPATA <pablo.zapata@epitech.eu>
2024-06-07 16:39:52 +0200

    feat(ai/network): created a ZappyServer class with connections logic

---
+++ ai/src/classes/zappy_server.lua
@@ -0,0 +1,52 @@
+--- @field protected host string
+--- @field protected port number
+--- @field protected tcp table
+--- @field protected status number
+--- @class ZappyServer
+local ZappyServer = {}
+
+local NetworkZappyAnswer <const> = require("ai/src/constants/network_zappy_answer")
+local NetworkTCPStatus <const> = require("ai/src/enums/network_tcp_status")
+local Socket <const> = require("socket")
+
+--- @param parent ZappyAI
+--- @param host string
+--- @param port number
+--- @return ZappyServer
+function ZappyServer.New(parent, host, port)
+    local self = setmetatable({}, {__index = ZappyServer})
+
+    self.parent = parent
+    self.host = host
+    self.port = port
+    self.tcp = Socket.tcp()
+    self.status = NetworkTCPStatus.IDLE
+    return self
+end
+
+--- @return number
+function ZappyServer:GetStatus()
+    return self.status
+end
+
+--- @return boolean
+function ZappyServer:Connect()
+    self.tcp:connect(self.host, self.port)
+    local answer <const> = self.tcp:receive()
+    if not answer or answer ~= NetworkZappyAnswer.CONNECTED then
+        return false
+    end
+    return true
+end
+
+--- @return boolean true if successfully disconnected
+function ZappyServer:Disconnect()
+    if self.status ~= NetworkTCPStatus.CONNECTED then
+        return false
+    end
+    self.tcp:close()
+    self.status = NetworkTCPStatus.INTERRUPTED
+    return true
+end
+
+return ZappyServer
