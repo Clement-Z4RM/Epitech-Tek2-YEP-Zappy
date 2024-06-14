b407f72df2a83fbb3dfadf33c64bff31084eb6f1
Arthur Bichara <arthur.bichara@epitech.eu>
2024-06-14 10:56:20 +0200

    feat(GUI): class Eggs and Egg

--- gui/src/egg.cpp
+++ gui/src/egg.cpp
@@ -6,3 +6,53 @@
 */
 
 #include "egg.hpp"
+
+bool Eggs::startsWith(const std::string &str, const std::string &prefix)
+{
+    return str.compare(0, prefix.size(), prefix) == 0;
+}
+
+bool Eggs::checkMsg(std::string &s)
+{
+    std::stringstream ss(s);
+    std::string str;
+    ss >> str;
+    if (startsWith(str, "enw")) {
+        addEgg(0, 0, 0);
+        return true;
+    }
+    if (startsWith(str, "edi")) {
+        deleteEgg(0);
+        return true;
+    }
+    if (startsWith(str, "eht")) {
+        deleteEgg(0);
+        return true;
+    }
+    if (startsWith(str, "pfk")) {
+        deleteEgg(0);
+        return true;
+    }
+    if (startsWith(str, "ebo")) {
+        deleteEgg(0);
+        return true;
+    }
+    return false;
+}
+
+Egg::Egg(int id, int x, int y) : _id(id), _x(x), _y(y) {}
+
+void Eggs::deleteEgg(int id)
+{
+    for (auto it = _eggs.begin(); it != _eggs.end(); it++) {
+        if (it->_id == id) {
+            _eggs.erase(it);
+            return;
+        }
+    }
+}
+
+void Eggs::addEgg(int id, int x, int y)
+{
+    _eggs.push_back(Egg(id, x, y));
+}
--- gui/src/egg.hpp
+++ gui/src/egg.hpp
@@ -7,3 +7,30 @@
 
 #pragma once
 
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+class Egg {
+public:
+    Egg() = default;
+    Egg(int id, int x, int y);
+    ~Egg() = default;
+
+    int _id;
+    int _x;
+    int _y;
+};
+
+class Eggs {
+public:
+    Eggs() = default;
+    ~Eggs() = default;
+    void addEgg(int id, int x, int y);
+    void deleteEgg(int id);
+    bool checkMsg(std::string &s);
+
+private:
+    std::vector<Egg> _eggs;
+    static bool startsWith(const std::string& str, const std::string& prefix);
+};
