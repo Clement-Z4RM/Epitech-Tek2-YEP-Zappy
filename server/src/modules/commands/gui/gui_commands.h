2ada68d1c84a368f4730d4d45ef25d794cf6ee8e
RemFdz <remi.fernandez@epitech.eu>
2024-06-17 15:12:00 +0200

    feat(server): msz command

---
+++ server/src/modules/commands/gui/gui_commands.h
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2024
+** Zappy-Server
+** File description:
+** command msz
+*/
+
+#pragma once
+#include <stdio.h>
+#include "client/client.h"
+#include "map/map.h"
+#include "macros.h"
+
+extern void msz(client_t *client, UNUSED client_manager_t *client_manager, map_t *map);
--- server/src/modules/commands/gui/msz.c
+++ server/src/modules/commands/gui/msz.c
@@ -5,9 +5,16 @@
 ** command msz
 */
 
-#include "../../client/client.h"
+#include "gui_commands.h"
 
-void msz(client_t *client)
+void msz(client_t *client, UNUSED client_manager_t *client_manager, map_t *map)
 {
-    (void)client;
+    char width[32];
+    char height[32];
+    char *response = NULL;
+
+    snprintf(width, sizeof(width), "%zu", map->width);
+    snprintf(height, sizeof(height), "%zu", map->height);
+    asprintf(&response, "msz %s %s\n", width, height);
+    client_add_request(client, response, TO_SEND);
 }
