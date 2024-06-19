e2d5134c86ca501748acecfb3508737179fdb807
RemFdz <remi.fernandez@epitech.eu>
2024-06-19 17:29:15 +0200

    refactor(server/gui): refactor pnw command for made ai_client node const

--- server/src/modules/commands/gui/events/pnw.c
+++ server/src/modules/commands/gui/events/pnw.c
@@ -7,18 +7,22 @@
 
 #include "../gui_commands.h"
 
-void pnw(ai_client_node_t *ai_client, client_manager_t *client_manager)
+void pnw(const ai_client_node_t *ai_client, const client_manager_t
+*client_manager)
 {
     gui_client_node_t *node = NULL;
-    player_t *player = &ai_client->player;
     char response[MAX_RESPONSE_SIZE];
 
     snprintf(
         response,
         MAX_RESPONSE_SIZE,
         "pnw #%lu %zu %zu %d %d %s\n",
-        player->id, player->x, player->y, player->direction,
-        player->level, ai_client->client->team_name
+        ai_client->player.id,
+        ai_client->player.x,
+        ai_client->player.y,
+        ai_client->player.direction,
+        ai_client->player.level,
+        ai_client->client->team_name
     );
     SLIST_FOREACH(node, &client_manager->gui_clients_list, next)
     {
