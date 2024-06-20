3987f1909dc60676213a6955cf106dc494de18d8
RemFdz <remi.fernandez@epitech.eu>
2024-06-20 23:16:00 +0200

    fix(server/gui): sst command fix not sending response

--- server/src/modules/commands/gui/handlers/sst.c
+++ server/src/modules/commands/gui/handlers/sst.c
@@ -11,6 +11,7 @@ void sst(gui_handler_data_t *data)
 {
     char *endptr = NULL;
     uint64_t t = strtol(data->args[1], &endptr, 10);
+    char response[MAX_RESPONSE_SIZE];
 
     if (endptr == data->args[1]) {
         sbp(data->gui_client->client);
@@ -20,5 +21,7 @@ void sst(gui_handler_data_t *data)
     data->updater->generation_interval = t * 20;
     data->updater->next_generation = data->updater->previous_time +
         data->updater->generation_interval;
+    snprintf(response, MAX_RESPONSE_SIZE, "sst %lu\n", t);
+    client_add_request(data->gui_client->client, response, TO_SEND);
     log_sst_success(data->gui_client->client);
 }
--- server/src/modules/logs/successes/gui_handlers/sst_success.c
+++ server/src/modules/logs/successes/gui_handlers/sst_success.c
@@ -7,11 +7,11 @@
 
 #include "../logs_successes.h"
 
-void log_sst_success(const client_t *client)
+void log_sst_success(const client_t *client, uint64_t t)
 {
     const char *ip = inet_ntoa(client->addr->sin_addr);
     const int port = ntohs(client->addr->sin_port);
 
-    LOG_SUCCESS("Sst command received from %s:%d executed successfully\n", ip,
-        port);
+    LOG_SUCCESS("Sst command received from %s:%d executed successfully, "
+    "new time unit: %lu\n", ip, port, t);
 }
