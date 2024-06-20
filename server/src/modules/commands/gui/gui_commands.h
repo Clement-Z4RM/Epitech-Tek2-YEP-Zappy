c71a7c5cf01da310ae46d31d6d8f2a7a2b6e5923
Clement-Z4RM <clement.lienard@epitech.eu>
2024-06-20 23:28:00 +0200

    refactor(gui-commands): return to line too long function prototypes

--- server/src/modules/commands/gui/gui_commands.h
+++ server/src/modules/commands/gui/gui_commands.h
@@ -29,12 +29,21 @@ extern void bct(gui_handler_data_t *data);
 extern void mct(gui_handler_data_t *data);
 
 /** EVENTS **/
-extern void plv(const player_t *player,
-    const clients_manager_t *clients_manager);
+extern void plv(
+    const player_t *player,
+    const clients_manager_t *clients_manager
+);
 extern void pfk(uint64_t id, const clients_manager_t *clients_manager);
-extern void pnw(const ai_client_node_t *ai_client, const clients_manager_t *clients_manager);
+extern void pnw(
+    const ai_client_node_t *ai_client,
+    const clients_manager_t *clients_manager
+);
 extern void pex(uint64_t id, const clients_manager_t *clients_manager);
-extern void pbc(uint64_t id, char *msg, const clients_manager_t *clients_manager);
+extern void pbc(
+    uint64_t id,
+    char *msg,
+    const clients_manager_t *clients_manager
+);
 extern void smg(char *msg, const clients_manager_t *clients_manager);
 extern void seg(char *team_name, clients_manager_t *clients_manager);
 extern void pdi(uint64_t id, clients_manager_t *clients_manager);
