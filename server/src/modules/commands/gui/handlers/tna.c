f81bd6abe526437ed0d3113028da46b578dae3d0
RemFdz <remi.fernandez@epitech.eu>
2024-06-20 18:19:12 +0200

    feat(server/gui): tna command

---
+++ server/src/modules/commands/gui/handlers/tna.c
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2024
+** Zappy-Server
+** File description:
+** tna.c
+*/
+
+#include "../gui_commands.h"
+
+static void send_team(char *name, client_t *client)
+{
+    char response[MAX_RESPONSE_SIZE];
+
+    snprintf(response, MAX_RESPONSE_SIZE, "tna %s\n", name);
+    client_add_request(client, response, TO_SEND);
+}
+
+void tna(gui_handler_data_t *data)
+{
+    team_node_t *current_team = NULL;
+
+    SLIST_FOREACH(current_team,
+        &data->updater->network->clients_manager->team_list,
+        next
+    )
+    {
+        send_team(current_team->name, data->gui_client->client);
+    }
+}
