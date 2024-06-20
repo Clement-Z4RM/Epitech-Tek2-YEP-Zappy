d303a9781eba07f936bc8d49816a51ea951f9c62
RemFdz <remi.fernandez@epitech.eu>
2024-06-20 18:14:13 +0200

    fix(server/gui): ppo send request to the gui

--- server/src/modules/commands/gui/handlers/ppo.c
+++ server/src/modules/commands/gui/handlers/ppo.c
@@ -10,21 +10,18 @@
 void ppo(gui_handler_data_t *data)
 {
     char *endptr = NULL;
-    uint64_t id = strtol(data->args[1], &endptr, 10);
+    const uint64_t id = strtol(data->args[1], &endptr, 10);
     ai_client_node_t *ai_client = NULL;
     char response[MAX_RESPONSE_SIZE];
 
-    if (*endptr != '\0') {
-        sbp(data->gui_client->client);
-        return;
-    }
+    if (*endptr != '\0')
+        return sbp(data->gui_client->client);
     ai_client = clients_manager_get_ai_by_id(data->updater
         ->network->clients_manager, id);
-    if (ai_client == NULL) {
-        sbp(data->gui_client->client);
-        return;
-    }
+    if (ai_client == NULL)
+        return sbp(data->gui_client->client);
     snprintf(response, MAX_RESPONSE_SIZE, "ppo %ld %lu %lu %d\n", ai_client
         ->player.id, ai_client->player.x, ai_client->player.y,
         ai_client->player.direction);
+    client_add_request(data->gui_client->client, response, TO_SEND);
 }
