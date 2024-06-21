b6adcb366857ea84b4bbadebe0234ae19d77f41b
Clement-Z4RM <clement.lienard@epitech.eu>
2024-06-21 12:42:56 +0200

    feat(server-clients-manager): add getter function to get the team of a client

--- server/src/modules/clients_manager/helpers/getters.c
+++ server/src/modules/clients_manager/helpers/getters.c
@@ -6,21 +6,36 @@
 */
 
 #include <stddef.h>
+#include <string.h>
 #include "../clients_manager.h"
 
 ai_client_node_t *clients_manager_get_ai_by_id(
-    clients_manager_t *manager, uint64_t id)
+    clients_manager_t *manager,
+    uint64_t id
+)
 {
     ai_client_node_t *node = NULL;
     team_node_t *current_team = NULL;
 
-    SLIST_FOREACH(current_team, &manager->team_list, next)
-    {
-        SLIST_FOREACH(node, &current_team->ai_clients, next)
-        {
+    SLIST_FOREACH(current_team, &manager->team_list, next) {
+        SLIST_FOREACH(node, &current_team->ai_clients, next) {
             if (node->player.id == id)
                 return node;
         }
     }
+    return NULL;
+}
+
+team_node_t *clients_manager_get_team_by_client(
+    clients_manager_t *manager,
+    ai_client_node_t *client
+)
+{
+    team_node_t *team;
+
+    SLIST_FOREACH(team, &manager->team_list, next)
+        if (team->name && client->client->team_name &&
+            !strcmp(team->name, client->client->team_name))
+            return team;
     return NULL;
 }
