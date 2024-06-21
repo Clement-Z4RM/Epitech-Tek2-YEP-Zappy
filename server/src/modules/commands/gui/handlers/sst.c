60ccdbdfe9e52a5d388ca85dae0a57a8cd93e248
Clement-Z4RM <clement.lienard@epitech.eu>
2024-06-21 13:28:50 +0200

    refactor(server-sst-command): change `t` type to `time_t` to avoid warning and/or problems

--- server/src/modules/commands/gui/handlers/sst.c
+++ server/src/modules/commands/gui/handlers/sst.c
@@ -6,10 +6,11 @@
 */
 
 #include "../gui_commands.h"
+
 void sst(gui_handler_data_t *data)
 {
     char *endptr = NULL;
-    uint64_t t = strtol(data->args[1], &endptr, 10);
+    time_t t = strtol(data->args[1], &endptr, 10);
 
     if (endptr == data->args[1]) {
         sbp(data->gui_client->client);
