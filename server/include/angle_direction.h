a4c994b3879db248dd4f3719e8569747f4e6d2bd
RemFdz <remi.fernandez@epitech.eu>
2024-06-21 18:47:34 +0200

    feat(server/broadcast): broadcast ai command

--- server/include/angle_direction.h
+++ server/include/angle_direction.h
@@ -8,7 +8,7 @@
 #pragma once
 #include "sys/types.h"
 typedef struct angle_direction_s {
-    int min_angle;
-    int max_angle;
+    double min_angle;
+    double max_angle;
     u_int8_t direction;
 } angle_direction_t;
--- server/src/modules/commands/ai/broadcast.c
+++ server/src/modules/commands/ai/broadcast.c
@@ -7,39 +7,69 @@
 
 #include "ai_commands.h"
 #include "math.h"
+#include "angle_direction.h"
+#include "responses.h"
 
-static ai_client_node_t **get_ai_node_tab(clients_manager_t *clients_manager)
+static int calc_cell_direction(int a)
 {
-    team_node_t *current_team = NULL;
-    ai_client_node_t *current_ai = NULL;
-    ai_client_node_t **ai_node_tab = malloc(
-        sizeof(ai_client_node_t *) * clients_manager->nb_ai_clients + 1
-    );
-    int i = 0;
+    int b = -1.5 * a * a * a + 13.5 * a * a - 37 * a + 33;
 
-    SLIST_FOREACH(current_team, &clients_manager->team_list, next)
-    {
-        SLIST_FOREACH(current_ai, &current_team->ai_clients, next)
-        {
-            ai_node_tab[i] = current_ai;
-            i++;
-        }
-    }
-    return ai_node_tab;
+    return b;
 }
 
-static int get_direction_tile(player_t *emitter, player_t *receiver)
+static int get_direction_tile(player_t *emitter, player_t *receiver, map_t *map)
 {
+    int o = receiver->direction;
+    double x_diff = (double)receiver->x - (double)emitter->x;
+    double y_diff = (double)receiver->y - (double)emitter->y;
+    angle_direction_t directions[] = {
+        {-M_PI / 8, M_PI / 8, calc_cell_direction(o)},
+        {M_PI / 8, 3 * M_PI / 8, calc_cell_direction(o) + 1},
+        {3 * M_PI / 8, 5 * M_PI / 8, calc_cell_direction(o) + 2},
+        {5 * M_PI / 8, 7 * M_PI / 8, calc_cell_direction(o) + 3},
+        {7 * M_PI / 8, 9 * M_PI / 8, calc_cell_direction(o) + 4},
+        {9 * M_PI / 8, 11 * M_PI / 8, calc_cell_direction(o) + 5},
+        {11 * M_PI / 8, 13 * M_PI / 8, calc_cell_direction(o) + 6},
+        {13 * M_PI / 8, 15 * M_PI / 8, calc_cell_direction(o) + 7},
+    };
+    double theta = 0;
 
+    if (x_diff > (double)map->width / 2)
+        x_diff -= (double)map->width;
+    else if (x_diff < (double)-map->width / 2)
+        x_diff += (double)map->width;
+    if (y_diff > (double)map->height / 2)
+        y_diff -= (double)map->height;
+    else if (y_diff < (double)-map->height / 2)
+        y_diff += (double)map->height;
+    theta = atan2(y_diff, x_diff);
+    for (unsigned long i = 0; i < sizeof(directions) / sizeof(directions[0]);
+        ++i)
+        if (theta >= directions[i].min_angle && theta < directions[i].max_angle)
+            return directions[i].direction;
+    return 0;
 }
 
 void broadcast(ai_handler_data_t *data)
 {
     char *msg = data->args[1];
-    ai_client_node_t **ai_node_tab = NULL;
+    team_node_t *current_team;
+    ai_client_node_t *current_ai;
+    int direction = 0;
+    char *response = malloc(MAX_RESPONSE_SIZE);
 
     if (msg == NULL)
         return;
-    ai_node_tab = get_ai_node_tab(data->clients_manager);
-    free(ai_node_tab);
+    SLIST_FOREACH(current_team, &data->clients_manager->team_list, next) {
+        SLIST_FOREACH(current_ai, &current_team->ai_clients, next) {
+            direction = get_direction_tile(
+                &data->client->player,
+                &current_ai->player,
+                data->map
+            );
+            snprintf(response, MAX_RESPONSE_SIZE, "message %d,%s\n",
+                direction, msg);
+            client_add_request(current_ai->client, response, TO_SEND);
+        }
+    }
 }
