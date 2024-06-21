9c3d10d77a673366f6fa75b3df35298b9b09a1df
RemFdz <remi.fernandez@epitech.eu>
2024-06-21 18:05:43 +0200

    feat(server/broadcast): angle direction struct

---
+++ server/include/angle_direction.h
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2024
+** Zappy-Server
+** File description:
+**
+*/
+
+#pragma once
+#include "sys/types.h"
+typedef struct angle_direction_s {
+    int min_angle;
+    int max_angle;
+    u_int8_t direction;
+} angle_direction_t;
