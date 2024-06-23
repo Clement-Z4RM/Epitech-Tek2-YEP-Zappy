d16d240746078324d9a6816fb7ab33fac7fa4675
Clement-Z4RM <clement.lienard@epitech.eu>
2024-06-23 12:07:47 +0200

    feat(server-incantation-ai-commands): send "Elevation underway" to all players in incantation at start

--- server/src/modules/commands/ai/incantation/incantation.c
+++ server/src/modules/commands/ai/incantation/incantation.c
@@ -8,6 +8,21 @@
 #include <string.h>
 #include "incantation.h"
 
+static void add_request_to_all_players(
+    incantation_t *incantation,
+    const char *request
+)
+{
+    incantation_player_t *player;
+
+    SLIST_FOREACH(player, &incantation->players, next)
+        client_add_request(
+            player->player->client,
+            strdup(request),
+            TO_SEND
+        );
+}
+
 static void incantation_updater(
     ai_client_node_t *client,
     UNUSED updater_t *updater,
@@ -33,12 +48,14 @@ void incantation(ai_handler_data_t *data)
         data->client,
         NULL
     };
-    incantation_t *incantation = initiate_incantation(data);
+    incantation_t *incantation = start_incantation(data);
 
     if (!incantation) {
         client_add_request(data->client->client, strdup("ko\n"), TO_SEND);
         return;
     }
+    add_request_to_all_players(incantation, "Elevation underway\n");
+    updater_data.arg = (void *)incantation;
     data->client->client->busy = true;
     updater_add_command(data->updater, &updater_data, incantation_updater);
 }
