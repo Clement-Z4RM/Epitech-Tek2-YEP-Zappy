56ee57fd56d44692a9567e28d79ede16483135ca
Pablo ZAPATA <pablo.zapata@epitech.eu>
2024-06-23 23:03:49 +0200

    feat(ai/utils): added Utils#GetActionsToTile method

--- ai/src/utils.lua
+++ ai/src/utils.lua
@@ -1,6 +1,8 @@
 --- @module Utils
 local Utils = {}
 
+local ZappyAction <const> = require("ai/src/constants/network_zappy_action")
+
 --- @param inputStr string
 --- @param sep string
 --- @return string[]
@@ -20,6 +22,56 @@ end
 --- @return boolean
 function Utils.StartsWith(str, prefix)
     return string.sub(str, 1, string.len(prefix)) == prefix
+end
+
+--- @param tileIndex number
+--- @return string[]
+function Utils.GetActionsToTile(tileIndex)
+    if tileIndex == 1 then
+        return {ZappyAction.MOVE_FORWARD, ZappyAction.MOVE_LEFT}
+    end
+    if tileIndex == 2 then
+        return {ZappyAction.MOVE_FORWARD}
+    end
+    if tileIndex == 3 then
+        return {ZappyAction.MOVE_FORWARD, ZappyAction.MOVE_RIGHT}
+    end
+    if tileIndex == 4 then
+        return {ZappyAction.MOVE_FORWARD, ZappyAction.MOVE_FORWARD, ZappyAction.MOVE_LEFT, ZappyAction.MOVE_LEFT}
+    end
+    if tileIndex == 5 then
+        return {ZappyAction.MOVE_FORWARD, ZappyAction.MOVE_FORWARD, ZappyAction.MOVE_LEFT}
+    end
+    if tileIndex == 6 then
+        return {ZappyAction.MOVE_FORWARD, ZappyAction.MOVE_FORWARD}
+    end
+    if tileIndex == 7 then
+        return {ZappyAction.MOVE_FORWARD, ZappyAction.MOVE_FORWARD, ZappyAction.MOVE_RIGHT}
+    end
+    if tileIndex == 8 then
+        return {ZappyAction.MOVE_FORWARD, ZappyAction.MOVE_FORWARD, ZappyAction.MOVE_RIGHT, ZappyAction.MOVE_RIGHT}
+    end
+    if tileIndex == 9 then
+        return {ZappyAction.MOVE_FORWARD, ZappyAction.MOVE_FORWARD, ZappyAction.MOVE_FORWARD, ZappyAction.MOVE_LEFT, ZappyAction.MOVE_LEFT, ZappyAction.MOVE_LEFT}
+    end
+    if tileIndex == 10 then
+        return {ZappyAction.MOVE_FORWARD, ZappyAction.MOVE_FORWARD, ZappyAction.MOVE_FORWARD, ZappyAction.MOVE_LEFT, ZappyAction.MOVE_LEFT}
+    end
+    if tileIndex == 11 then
+        return {ZappyAction.MOVE_FORWARD, ZappyAction.MOVE_FORWARD, ZappyAction.MOVE_FORWARD, ZappyAction.MOVE_LEFT}
+    end
+    if tileIndex == 12 then
+        return {ZappyAction.MOVE_FORWARD, ZappyAction.MOVE_FORWARD, ZappyAction.MOVE_FORWARD}
+    end
+    if tileIndex == 13 then
+        return {ZappyAction.MOVE_FORWARD, ZappyAction.MOVE_FORWARD, ZappyAction.MOVE_FORWARD, ZappyAction.MOVE_RIGHT}
+    end
+    if tileIndex == 14 then
+        return {ZappyAction.MOVE_FORWARD, ZappyAction.MOVE_FORWARD, ZappyAction.MOVE_FORWARD, ZappyAction.MOVE_RIGHT, ZappyAction.MOVE_RIGHT}
+    end
+    if tileIndex == 15 then
+        return {ZappyAction.MOVE_FORWARD, ZappyAction.MOVE_FORWARD, ZappyAction.MOVE_FORWARD, ZappyAction.MOVE_RIGHT, ZappyAction.MOVE_RIGHT, ZappyAction.MOVE_RIGHT}
+    end
 end
 
 return Utils
